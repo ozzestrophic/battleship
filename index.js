1929d03dbef5925327a08c6f3fe1643aa2d43a3a
Ozzeek <osama.m.alnagar@gmail.com>
2024-01-09 22:17:23 +0200

    feat: GameBoard now placesShips using coords

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 class Ship {
-  constructor(length) {
+  constructor(length, head, tail) {
     this.length = length;
     this.hits = 0;
     this.sunk = false;
+    this.horzontal = true; // maybe use this for quick conditionals when calculating hits coordinates or EVEN PLACEMENT in GUI
+    if (
+      (tail[0] - head[0] === length - 1 && head[1] === tail[1]) ||
+      (tail[1] - head[1] === length - 1 && head[0] === tail[0])
+    ) {
+      this.head = head;
+      this.tail = tail;
+    } else {
+      throw new Error("wrong coords");
+    }
   }
 
   hit() {
@@ -18,4 +28,22 @@ class Ship {
   }
 }
 
-export { Ship };
+class Gameboard {
+  constructor(...args) {
+    // this.ship0;
+    // this.ship1;
+    // this.ship2;
+    this.missedShots = [];
+    this.placeShips(args);
+  }
+
+  placeShips(args) {
+    for (let i = 0; i < args.length; i++) {
+      this[`ship${i}`] = new Ship(...args[i]);
+    }
+  }
+
+  recieveAttack() {}
+}
+
+export { Ship, Gameboard };
--- index.test.js
+++ index.test.js
@@ -1,23 +1,48 @@
-import { Ship } from "./index.js";
+import { Ship, Gameboard } from "./index.js";
+
+test("coordinates works horizontally", () => {
+  const ship = new Ship(3, [2, 1], [4, 1]);
+
+  expect(ship.head).toEqual([2, 1]);
+});
+
+test("coordinates works vertically", () => {
+  const ship = new Ship(4, [2, 1], [2, 4]);
+
+  expect(ship.head).toEqual([2, 1]);
+});
 
 test("length works", () => {
-  const ship = new Ship(2);
+  const ship = new Ship(2, [2, 1], [3, 1]);
 
   expect(ship.length).toBe(2);
 });
 
 test("hits works", () => {
-  const ship = new Ship(3);
+  const ship = new Ship(3, [2, 1], [4, 1]);
   ship.hit();
 
   expect(ship.hits).toBe(1);
 });
 
 test("sunk works", () => {
-  const ship = new Ship(3);
+  const ship = new Ship(3, [2, 1], [4, 1]);
   ship.hit();
   ship.hit();
   ship.hit();
 
   expect(ship.isSunk()).toBe(true);
+});
+
+describe("test gameboard", () => {
+  //   test("gamboard recieve attack", () => {
+  //     gameBoard.recieveAttack([3, 2]);
+
+  //     expect();
+  //   });
+
+  test("gameBoard places ships", () => {
+    const gameBoard = new Gameboard([3, [2, 2], [4, 2]], [4, [4, 4], [7, 4]]);
+    expect(gameBoard.ship1.length).toEqual(4);
+  });
 });
